47cae3d070da087ccd29a75ef88be9236a3cf3dd
JANG <kennethjang34@gmail.com>
2022-12-31 21:53:54 -0800

    add abstraction for writing into VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -11,13 +12,15 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(
+        vga_buffer::WRITER.lock(),
+        ", some numbers: {} {} ",
+        42,
+        1.337
+    )
+    .unwrap();
 
     loop {}
 }
