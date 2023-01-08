946a612ff119de216f91c975b3639b473aa6acb5
JANG <kennethjang34@gmail.com>
2023-01-08 10:41:44 -0800

    add CPU interrupt config

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
+pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 
@@ -41,9 +43,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hellow World{}", "!");
-
-    #[cfg(test)]
+    init();
     test_main();
     loop {}
 }
@@ -68,4 +68,8 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
+}
+
+pub fn init() {
+    interrupts::init_idt();
 }
--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,11 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+    rust_os::init();
+    x86_64::instructions::interrupts::int3();
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
