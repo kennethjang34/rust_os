1ee959587d3a2a707a5a0304d8bca3160d2d189a
JANG <kennethjang34@gmail.com>
2023-01-23 22:25:02 -0800

    add functions for translating virtual memory addresses to their mapped physical addresses

--- src/main.rs
+++ src/main.rs
@@ -24,45 +24,25 @@ entry_point!(kernel_main);
 #[no_mangle]
 //boot_info can be skipped since the x86_64 convention passes it in a cpu register instead of in stack
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use rust_os::memory::active_layer_4_table;
-    use x86_64::structures::paging::PageTable;
+    use rust_os::memory::translate_addr;
     use x86_64::VirtAddr;
 
     println!("Hello World{}", "!");
     rust_os::init();
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_layer_4_table(phys_mem_offset) };
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!(" L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
+    let addresses = [
+        0xb8000,                          // VGA buffer
+        0x201008,                         // a code page
+        0x0100_0020_1a10,                 // a stack page
+        boot_info.physical_memory_offset, //virtual address pointing to the start of the physical memory (i.e. physical memory address 0)
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
     }
 
-    use x86_64::registers::control::Cr3;
-
-    let (level_4_page_table, _) = Cr3::read();
-    println!(
-        "Level 4 page table at: {:?}",
-        level_4_page_table.start_address()
-    );
-
-    //fn stack_overflow() {
-    //stack_overflow();
-    //}
-    //stack_overflow();
-    //unsafe {
-    //*(0xdeadbeef as *mut u64) = 42;
-    //};
-    //x86_64::instructions::interrupts::int3();
     #[cfg(test)]
     test_main();
 
